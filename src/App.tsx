70505ce69dfa1b48dc760076676afde76881276d
t-kida-tribeat <t-kida@tribeat.com>
2023-12-27 18:22:05 +0900

    useContext

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,40 @@
 import './App.css'
-import { useState } from 'react'
+import { useContext, useEffect, useState } from 'react'
+import context from './main'
 
 function App() {
 
-
   const [count, setCount] = useState(0)
+  const info = useContext(context)
 
   const handleClick = () => {
     setCount(count + 1)
   }
 
+  useEffect(() => {
+
+    console.log("Hello Hooks")
+  }, [count])
+
   return (
     <div className='App'>
-      <section className='useState'>
-        <h1>UseState</h1>
+      {/* useState, useEffect */}
+      <section>
+        <h1>useState, useEffect</h1>
 
         <p>{count}</p>
         <button onClick={handleClick}>+</button>
       </section>
+      {/* useState, useEffect */}
+      <hr />
+      {/* useContext */}
+      <section>
+        <h1>useContext</h1>
+        <p>{info.name}</p>
+        <p>{info.age}</p>
+      </section>
+      {/* useContext */}
+
 
     </div>
   )
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,22 @@
-import React from 'react'
+import React, { createContext } from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+const info = {
+  name: 'yukiice',
+  age: 18
+}
+
+const context = createContext(info)
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <context.Provider value={info}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </context.Provider>
+
 )
+
+export default context;
