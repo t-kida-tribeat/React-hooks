6f8bf55bb36c4915891db7dc42219c66c2681246
t-kida-tribeat <t-kida@tribeat.com>
2023-12-27 18:33:38 +0900

    useRef

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import './App.css'
-import { useContext, useEffect, useState } from 'react'
+import { useContext, useEffect, useState, useRef } from 'react'
 import context from './main'
 
 function App() {
 
   const [count, setCount] = useState(0)
   const info = useContext(context)
+  const ref = useRef(null)
 
   const handleClick = () => {
     setCount(count + 1)
@@ -15,6 +16,10 @@ function App() {
 
     console.log("Hello Hooks")
   }, [count])
+
+  const handleRef = () => {
+    console.log(ref)
+  }
 
   return (
     <div className='App'>
@@ -32,8 +37,24 @@ function App() {
         <h1>useContext</h1>
         <p>{info.name}</p>
         <p>{info.age}</p>
+        <p>
+          親コンポーネントから子孫コンポーネントに値を渡すときに使う。<br />
+          propsでも渡せるが親から孫に渡すときにはpropsを何回も渡さなければならない。<br />
+          そのため、useContextを使うと親から孫に値を渡すことができる。
+        </p>
       </section>
       {/* useContext */}
+      <hr />
+      {/* useRef */}
+      <section>
+        <h1>useRef</h1>
+        <input type="text" ref={ref} />
+        <button onClick={handleRef}>ref</button>
+        <p>
+          inputタグの情報を取得するときに使う。<br />
+        </p>
+      </section>
+      {/* useRef */}
 
 
     </div>
